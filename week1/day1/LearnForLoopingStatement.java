510c68042438af3c70afc97fa6661f4f8e9d90a4
sathish9566 <96668153+sathish9566@users.noreply.github.com>
2022-01-03 10:18:02 +0530

    Add files via upload

---
+++ week1/day1/LearnForLoopingStatement.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class LearnForLoopingStatement {
+
+	public static void main(String[] args) {
+
+		// to run the same code multiple time use for loop
+		
+		// initialize ; condition ; increment / decrement
+		
+		for(int i = 15; i >= 1; i--) {
+			System.out.println(i);
+		}
+		
+
+		
+
+	}
+
+}
